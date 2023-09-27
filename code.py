7fd5b40ba443f94f6c3a87bde64ba00ec0415054
TheODDYSEY <mosessabz07@gmail.com>
2023-09-27 23:57:26 +0300

    Packaages added

---
+++ code.py
@@ -0,0 +1,8 @@
+from transformers import SpeechT5Processor, SpeechT5ForTextToSpeech, SpeechT5HifiGan
+from datasets import load_dataset
+import torch
+import random
+import string
+import soundfile as sf
+
+device = "cuda" if torch.cuda.is_available() else "cpu"
